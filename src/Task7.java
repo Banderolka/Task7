55f1984d81a090cd8af1d214efdd85de7fbaff95
OlloOllo <bothisava@gmail.com>
2019-07-16 22:14:47 +0300

    задание 7

---
+++ src/Task7.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число");
+        int number = Integer.parseInt(reader.readLine());
+        final int X = 3;
+        final int Y = 5;
+        final int Z = 7;
+        if (number == X || number == Y || number == Z){
+            System.out.println("Данное значение имеется в константах");
+        }
+        else {
+            System.out.println("Такой константы нет!");
+        }
+    }
+}
